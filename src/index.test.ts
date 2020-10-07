73f6a20a1c810d95dbdebd3e47d76f4e6c2b6875
Aaron Osher <git@aaronosher.io>
2020-10-07 12:37:21 +0100

    Fix todo tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,9 @@
 describe('update', () => {
-  xit('Should set the channel name to meeting in progress if there is a meeting in progress');
-  xit(
+  xit.todo('Should set the channel name to meeting in progress if there is a meeting in progress');
+  xit.todo(
     'Should set the channel name to meeting in progress if there is a meeting in progress and a meeting within 60 minutes.',
   );
-  xit('Should reset the channel name if there are no upcoming meetings');
-  xit('Should set the chanel name to meeting at HH:mm if there is a meeting in the next 60 minutes');
-  xit('Should reset the channel name if the next meeting is in more than 60 minutes');
+  xit.todo('Should reset the channel name if there are no upcoming meetings');
+  xit.todo('Should set the chanel name to meeting at HH:mm if there is a meeting in the next 60 minutes');
+  xit.todo('Should reset the channel name if the next meeting is in more than 60 minutes');
 });
